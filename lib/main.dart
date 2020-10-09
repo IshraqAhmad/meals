ae006e76e650e137bb95ef69f918f595ea7a55c3
Ishraq Ahmad <ishraqahmad@Ishraqs-MacBook-Pro.local>
2020-10-08 21:06:42 -0700

    Added meals detail.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './screens/meals_detail_screen.dart';
 import './screens/category_meals_screen.dart';
 import './models/category.dart';
 import './screens/categories_screen.dart';
@@ -9,30 +10,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
-      theme: ThemeData(
-          primarySwatch: Colors.pink,
-          accentColor: Colors.amber,
-          canvasColor: Color.fromRGBO(255, 254, 229, 1),
-          fontFamily: 'Raleway',
-          textTheme: ThemeData.light().textTheme.copyWith(
-              body1: TextStyle(
-                color: Color.fromRGBO(20, 51, 51, 1),
-              ),
-              body2: TextStyle(
-                color: Color.fromRGBO(20, 51, 51, 1),
-              ),
-              title: TextStyle(
-                fontSize: 20,
-                fontFamily: 'RobotoCondensed',
-                fontWeight: FontWeight.bold,
-              ))),
-      // home: CategoriesScreen(),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => CategoriesScreen(),
-        CategoryMealsScreen.routeName: (context) => CategoryMealsScreen(),
-      },
-    );
+        title: 'DeliMeals',
+        theme: ThemeData(
+            primarySwatch: Colors.pink,
+            accentColor: Colors.amber,
+            canvasColor: Color.fromRGBO(255, 254, 229, 1),
+            fontFamily: 'Raleway',
+            textTheme: ThemeData.light().textTheme.copyWith(
+                body1: TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                body2: TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                title: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.bold,
+                ))),
+        // home: CategoriesScreen(),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => CategoriesScreen(),
+          CategoryMealsScreen.routeName: (context) => CategoryMealsScreen(),
+          MealDetailScreen.routeName: (context) => MealDetailScreen(),
+        },
+        // onGenerateRoute: (settings) {
+        //   print(settings.arguments);
+        //   return MaterialPageRoute(builder: (context) => CategoriesScreen());
+        // },
+        onUnknownRoute: (settings) {
+          return MaterialPageRoute(builder: (context) => CategoriesScreen());
+        });
   }
 }
